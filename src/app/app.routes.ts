f967c15bc17d5ff1e6df4a374ecdbd534d5d0c78
Aravind Pavan Kalyan <98452386+AravindPavanKalyan@users.noreply.github.com>
2025-09-11 11:04:45 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { ServicesProvided } from './pages/services-provided/services-provided';
+import { Service1 } from './pages/services-provided/service1/service1';
+import { Service2 } from './pages/services-provided/service2/service2';
+import { Service3 } from './pages/services-provided/service3/service3';
 import { Product } from './pages/product/product';
 import { Industries } from './pages/industries/industries';
 import { Projects } from './pages/projects/projects';
@@ -12,7 +15,15 @@ import { Contact } from './pages/contact/contact';
 export const routes: Routes = [
   { path: '', component: Home, pathMatch: 'full' },
   { path: 'about', component: About },
-  { path: 'services', component: ServicesProvided },
+  {
+  path: 'services',
+  component: ServicesProvided,
+    children: [
+      { path: 'service1', component: Service1 },
+      { path: 'service2', component: Service2 },
+      { path: 'service3', component: Service3 },
+    ]
+  },
   { path: 'product', component: Product },
   { path: 'industries', component: Industries },
   { path: 'projects', component: Projects },

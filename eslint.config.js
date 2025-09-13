0c27aec842d8b7003b3cf41d6b2a254335da3227
Aravind Pavan Kalyan <98452386+AravindPavanKalyan@users.noreply.github.com>
2025-09-14 01:34:01 +0530

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/.angular/**',
+      '**/dist/**',
+      '**/.output/**',
+      '**/.cache/**',
+      '**/.next/**',
+      '**/.vercel/**',
+    ],
+  },
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);

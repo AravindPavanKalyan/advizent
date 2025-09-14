d8dfdfc35628171e28562f6b292fa061f6743a7a
Aravind Pavan Kalyan <98452386+AravindPavanKalyan@users.noreply.github.com>
2025-09-14 15:38:30 +0530

    Update home.ts

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SERVICES } from './services-data';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-
+  services = SERVICES;
 }

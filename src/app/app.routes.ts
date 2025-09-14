ef69a2130693efeaa67fbb121b26c0157c171377
AravindPavanKalyan <aravindpavankalyan1996@gmail.com>
2025-09-14 16:12:45 +0530

    routes changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { ServicesProvided } from './pages/services-provided/services-provided';
-import { DraftingAndBIM } from './pages/services-provided/drafting-and-bim/drafting-and-bim';
-import { ScanToBIM } from './pages/services-provided/scan-to-bim/scan-to-bim';
-import { ThreeDCADModels } from './pages/services-provided/3d-cad-models/3d-cad-models';
+import { DraftingAndBim } from './pages/services-provided/drafting-and-bim/drafting-and-bim';
+import { ScanToBim } from './pages/services-provided/scan-to-bim/scan-to-bim';
+import { ThreeDCadModels } from './pages/services-provided/three-d-cad-models/three-d-cad-models';
 import { ScanRegistration } from './pages/services-provided/scan-registration/scan-registration';
-import { Plant3DModel } from './pages/services-provided/plant-3d-model/plant-3d-model';
+import { PlantThreeDModel } from './pages/services-provided/plant-three-d-model/plant-three-d-model';
 import { ReverseEngineering } from './pages/services-provided/reverse-engineering/reverse-engineering';
-import { ThreeDLaserScanning } from './pages/services-provided/3d-laser-scanning/3d-laser-scanning';
+import { ThreeDLaserScanning } from './pages/services-provided/three-d-laser-scanning/three-d-laser-scanning';
 import { TrainingServices } from './pages/services-provided/training-services/training-services';
 import { Product } from './pages/product/product';
 import { Industries } from './pages/industries/industries';
@@ -24,11 +24,11 @@ export const routes: Routes = [
   path: 'services',
   component: ServicesProvided,
     children: [
-      { path: 'drafting-and-bim', component: DraftingAndBIM },
-      { path: 'scan-to-bim', component: ScanToBIM },
-      { path: '3d-cad-models', component: ThreeDCADModels },
+      { path: 'drafting-and-bim', component: DraftingAndBim },
+      { path: 'scan-to-bim', component: ScanToBim },
+      { path: '3d-cad-models', component: ThreeDCadModels },
       { path: 'scan-registration', component: ScanRegistration },
-      { path: 'plant-3d-model', component: Plant3DModel },
+      { path: 'plant-3d-model', component: PlantThreeDModel },
       { path: 'reverse-engineering', component: ReverseEngineering },
       { path: '3d-laser-scanning', component: ThreeDLaserScanning },
       { path: 'training-services', component: TrainingServices },

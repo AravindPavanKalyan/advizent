06633ca446aeff5c2a72e0a48ef68ac13310a0be
Aravind Pavan Kalyan <98452386+AravindPavanKalyan@users.noreply.github.com>
2025-09-22 01:20:22 +0530

    Update home.ts

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SERVICES } from './services-data';
+import { PhotoCarouselComponent } from '../../components/photo-carousel/photo-carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, PhotoCarouselComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
